9c3e0cd108252dba53ec336730061832ad495c0c
profedonbelem <profedsonbelem@gmail.com>
2020-10-08 18:14:35 -0300

    Atualizacao

--- Contrato.js
+++ Contrato.js
@@ -1,22 +1,108 @@
 const mongoose = require("mongoose");
-
+const moment = require('moment');
 
 const schema = mongoose.Schema({
-    "subcontrato": {
+  "subcontrato": {
+    "id": {
+      "type": "Number"
+    }
+  },
+  "contrato": {
+    "id": {
+      "type": "Number"
+    }
+  },
+  "dataVigencia": {
+    "type": "Date"
+  },
+  "dataVencimento": {
+    "type": "Date"
+  },
+  "operadora": {
+    "id": {
+      "type": "Number"
+    },
+    "cnpj": {
+      "type": "String"
+    },
+    "nome": {
+      "type": "String"
+    },
+    "site": {
+      "type": "String"
+    },
+    "telefone": {
+      "type": "String"
+    }
+  },
+  "entidade": {
+    "id": {
+      "type": "Number"
+    },
+    "sigla": {
+      "type": "String"
+    },
+    "nome": {
+      "type": "String"
+    }
+  },
+  "plano": {
+    "id": {
+      "type": "Number"
+    },
+    "nome": {
+      "type": "String"
+    },
+    "codigoANS": {
+      "type": "String"
+    },
+    "tipoContratacao": {
       "id": {
         "type": "Number"
+      },
+      "descricao": {
+        "type": "String"
       }
     },
-    "contrato": {
+    "acomodacao": {
       "id": {
         "type": "Number"
+      },
+      "descricao": {
+        "type": "String"
       }
     },
-    "dataVigencia": {
-      "type": "Date"
+    "fatorModerador": {
+      "id": {
+        "type": "Number"
+      },
+      "descricao": {
+        "type": "String"
+      }
     },
-    "dataVencimento": {
-      "type": "Date"
+    "cobertura": {
+      "id": {
+        "type": "Number"
+      },
+      "descricao": {
+        "type": "String"
+      }
+    },
+    "abrangenciaGeografica": {
+      "id": {
+        "type": "Number"
+      },
+      "descricao": {
+        "type": "String"
+      }
+    },
+    "areaAtuacao": {
+      "id": {
+        "type": "Number"
+      },
+      "descricao": {
+        "type": "String"
+      }
     },
     "operadora": {
       "id": {
@@ -35,282 +121,217 @@ const schema = mongoose.Schema({
         "type": "String"
       }
     },
-    "entidade": {
+    "adicionais": {
+      "type": "Mixed"
+    },
+    "formacaoDePreco": {
+      "type": "Mixed"
+    },
+    "quantidadeVidas": {
+      "type": "Number"
+    },
+    "valorTotal": {
+      "type": "Mixed"
+    },
+    "valorTotalTabela": {
+      "type": "Mixed"
+    },
+    "recorrenciaPromocaoMeses": {
+      "type": "Mixed"
+    },
+    "indPromocional": {
+      "type": "Mixed"
+    },
+    "indPossuiOpcional": {
+      "type": "Boolean"
+    }
+  },
+  titular: {
+    "id": {
+      "type": "Number"
+    },
+    "cpf": {
+      "type": "String"
+    },
+    "rg": {
+      "type": "String"
+    },
+    "nome": {
+      "type": "String"
+    },
+    dataNascimento: {
+      "type": "Mixed"
+    },
+    "nomeMae": {
+      "type": "String"
+    },
+    "sexo": {
+      "type": "String"
+    },
+    "cns": {
+      "type": "Mixed"
+    },
+    "dnv": {
+      "type": "Mixed"
+    },
+    "email": {
+      "type": "String"
+    },
+    "dddTelefone": {
+      "type": "Mixed"
+    },
+    "numTelefone": {
+      "type": "Mixed"
+    },
+    "dddCelular": {
+      "type": "String"
+    },
+    "numCelular": {
+      "type": "String"
+    },
+    "valor": {
+      "type": "Number"
+    },
+    "idade": {
+      "type": "Number"
+    },
+    "numeroCarteirinha": {
+      "type": "Mixed"
+    },
+    "estadoCivil": {
       "id": {
         "type": "Number"
       },
-      "sigla": {
-        "type": "String"
-      },
-      "nome": {
+      "descricao": {
         "type": "String"
       }
     },
-    "plano": {
+    "orgaoEmissor": {
       "id": {
         "type": "Number"
       },
-      "nome": {
+      "descricao": {
         "type": "String"
-      },
-      "codigoANS": {
-        "type": "String"
-      },
-      "tipoContratacao": {
-        "id": {
-          "type": "Number"
-        },
-        "descricao": {
-          "type": "String"
-        }
-      },
-      "acomodacao": {
-        "id": {
-          "type": "Number"
-        },
-        "descricao": {
-          "type": "String"
-        }
-      },
-      "fatorModerador": {
-        "id": {
-          "type": "Number"
-        },
-        "descricao": {
-          "type": "String"
-        }
-      },
-      "cobertura": {
-        "id": {
-          "type": "Number"
-        },
-        "descricao": {
-          "type": "String"
-        }
-      },
-      "abrangenciaGeografica": {
-        "id": {
-          "type": "Number"
-        },
-        "descricao": {
-          "type": "String"
-        }
-      },
-      "areaAtuacao": {
-        "id": {
-          "type": "Number"
-        },
-        "descricao": {
-          "type": "String"
-        }
-      },
-      "operadora": {
-        "id": {
-          "type": "Number"
-        },
-        "cnpj": {
-          "type": "String"
-        },
-        "nome": {
-          "type": "String"
-        },
-        "site": {
-          "type": "String"
-        },
-        "telefone": {
-          "type": "String"
-        }
-      },
-      "adicionais": {
-        "type": "Mixed"
-      },
-      "formacaoDePreco": {
-        "type": "Mixed"
-      },
-      "quantidadeVidas": {
-        "type": "Number"
-      },
-      "valorTotal": {
-        "type": "Mixed"
-      },
-      "valorTotalTabela": {
-        "type": "Mixed"
-      },
-      "recorrenciaPromocaoMeses": {
-        "type": "Mixed"
-      },
-      "indPromocional": {
-        "type": "Mixed"
-      },
-      "indPossuiOpcional": {
-        "type": "Boolean"
       }
     },
-    "titular": {
+    "endereco": {
       "id": {
         "type": "Number"
       },
-      "cpf": {
+      "cep": {
         "type": "String"
       },
-      "rg": {
+      "logradouro": {
         "type": "String"
       },
-      "nome": {
+      "numero": {
+        "type": "Date"
+      },
+      "complemento": {
         "type": "String"
       },
-      "dataNascimento": {
-        "type": "Mixed"
-      },
-      "nomeMae": {
+      "bairro": {
         "type": "String"
       },
-      "sexo": {
+      "cidade": {
         "type": "String"
       },
-      "cns": {
-        "type": "Mixed"
-      },
-      "dnv": {
-        "type": "Mixed"
-      },
-      "email": {
+      "uf": {
         "type": "String"
-      },
-      "dddTelefone": {
-        "type": "Mixed"
-      },
-      "numTelefone": {
-        "type": "Mixed"
-      },
-      "dddCelular": {
-        "type": "String"
-      },
-      "numCelular": {
-        "type": "String"
-      },
-      "valor": {
-        "type": "Number"
-      },
-      "idade": {
-        "type": "Number"
-      },
-      "numeroCarteirinha": {
-        "type": "Mixed"
-      },
-      "estadoCivil": {
-        "id": {
-          "type": "Number"
-        },
-        "descricao": {
-          "type": "String"
-        }
-      },
-      "orgaoEmissor": {
-        "id": {
-          "type": "Number"
-        },
-        "descricao": {
-          "type": "String"
-        }
-      },
-      "endereco": {
-        "id": {
-          "type": "Number"
-        },
-        "cep": {
-          "type": "String"
-        },
-        "logradouro": {
-          "type": "String"
-        },
-        "numero": {
-          "type": "Date"
-        },
-        "complemento": {
-          "type": "String"
-        },
-        "bairro": {
-          "type": "String"
-        },
-        "cidade": {
-          "type": "String"
-        },
-        "uf": {
-          "type": "String"
-        }
-      },
-      "representanteLegal": {
-        "type": "Mixed"
-      },
-      "carencia": {
-        "type": "Mixed"
-      },
-      "dependente": {
-        "type": [
-          "Mixed"
-        ]
-      },
-      "profissao": {
-        "id": {
-          "type": "Number"
-        },
-        "descricao": {
-          "type": "String"
-        }
       }
     },
-    "corretora": {
-      "id": {
-        "type": "Number"
-      },
-      "cnpj": {
-        "type": "String"
-      },
-      "nome": {
-        "type": "String"
-      },
-      "razaoSocial": {
-        "type": "Mixed"
-      },
-      "possuiSupervisor": {
-        "type": "Boolean"
-      },
-      "codigo": {
-        "type": "String"
-      },
-      "email": {
-        "type": "String"
-      },
-      "indAtivo": {
-        "type": "Mixed"
-      },
-      "indLiberacaoColaborador": {
-        "type": "Mixed"
-      }
+    "representanteLegal": {
+      "type": "Mixed"
     },
-    "administradora": {
+    "carencia": {
+      "type": "Mixed"
+    },
+    "dependente": {
+      "type": [
+        "Mixed"
+      ]
+    },
+    "profissao": {
       "id": {
         "type": "Number"
       },
-      "codigoANS": {
-        "type": "String"
-      },
-      "site": {
-        "type": "String"
-      },
-      "telefone": {
-        "type": "String"
-      },
-      "cnpj": {
-        "type": "String"
-      },
-      "nomeFantasia": {
-        "type": "String"
-      },
-      "razaoSocial": {
+      "descricao": {
         "type": "String"
       }
+    }
+  },
+  "corretora": {
+    "id": {
+      "type": "Number"
+    },
+    "cnpj": {
+      "type": "String"
+    },
+    "nome": {
+      "type": "String"
+    },
+    "razaoSocial": {
+      "type": "Mixed"
+    },
+    "possuiSupervisor": {
+      "type": "Boolean"
+    },
+    "codigo": {
+      "type": "String"
+    },
+    "email": {
+      "type": "String"
+    },
+    "indAtivo": {
+      "type": "Mixed"
+    },
+    "indLiberacaoColaborador": {
+      "type": "Mixed"
+    }
+  },
+  "administradora": {
+    "id": {
+      "type": "Number"
+    },
+    "codigoANS": {
+      "type": "String"
+    },
+    "site": {
+      "type": "String"
+    },
+    "telefone": {
+      "type": "String"
+    },
+    "cnpj": {
+      "type": "String"
+    },
+    "nomeFantasia": {
+      "type": "String"
+    },
+    "razaoSocial": {
+      "type": "String"
+    }
+  },
+  "corretor": {
+    "id": {
+      "type": "Number"
+    },
+    "cpf": {
+      "type": "String"
+    },
+    "nome": {
+      "type": "String"
+    },
+    "email": {
+      "type": "String"
+    },
+    "celular": {
+      "type": "String"
+    }
+  },
+  "supervisor": {
+    "id": {
+      "type": "Number"
     },
     "corretor": {
       "id": {
@@ -326,58 +347,42 @@ const schema = mongoose.Schema({
         "type": "String"
       },
       "celular": {
-        "type": "String"
-      }
-    },
-    "supervisor": {
-      "id": {
-        "type": "Number"
-      },
-      "corretor": {
-        "id": {
-          "type": "Number"
-        },
-        "cpf": {
-          "type": "String"
-        },
-        "nome": {
-          "type": "String"
-        },
-        "email": {
-          "type": "String"
-        },
-        "celular": {
-          "type": "Mixed"
-        }
-      },
-      "corretora": {
         "type": "Mixed"
       }
     },
-    "valorTotal": {
-      "type": "Number"
-    },
-    "valorTaxaAssociativa": {
-      "type": "Number"
-    },
-    "taxaCadastro": {
-      "type": "Number"
-    },
-    "valorTotalMensal": {
-      "type": "Number"
-    },
-    "numeroProposta": {
-      "type": "String"
-    },
-    "qtdRecorrenciaAnualTaxaAssociativa": {
+    "corretora": {
       "type": "Mixed"
-    },
-    "descricaoRecorrenciaTaxaAssociativa": {
-      "type": "String"
     }
-  },{collection:'mensage'});
+  },
+  "valorTotal": {
+    "type": "Number"
+  },
+  "valorTaxaAssociativa": {
+    "type": "Number"
+  },
+  "taxaCadastro": {
+    "type": "Number"
+  },
+  "valorTotalMensal": {
+    "type": "Number"
+  },
+  "numeroProposta": {
+    "type": "String"
+  },
+  "qtdRecorrenciaAnualTaxaAssociativa": {
+    "type": "Mixed"
+  },
+  "descricaoRecorrenciaTaxaAssociativa": {
+    "type": "String"
+  }
+}, { collection: 'mensage' });
+
+schema.pre('save', function (next) {
+  this.titular.dataNascimento = moment().format('YYYY-MM-DD');
+  next();
+
+})
 
 
-  const Contrato = mongoose.model('contratos', schema);
-  module.exports = Contrato;
-  
+const Contrato = mongoose.model('contratos', schema);
+module.exports = Contrato;
--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 // require('./utils/redis')
 const cors = require("cors");
-const User = require("./controller/user");
 var app = express();
+const ICliente = require('./entity/InterfacedoCliente');
 const https = require('https');
 const fs = require('fs');
 const { credentials } = require("amqplib/callback_api");
@@ -55,136 +55,137 @@ var createContrato = function (req, res, next) {
 
 	let cliente = req.body;
 	let dataNascimento = moment(cliente.titular.dataNascimento).format('DD/MM/YYYY')
-	var contrato = new Contrato(req.body);
+	let contrato = new Contrato(req.body);
+	
 
 	contrato.save(async function (err) {
 
 		if (err) {
 			next(err);
 		} else {
-			await axios.post('https://prjqualivida.mxmwebmanager.com.br/api/InterfacedoCliente/Gravar', {
-				AutheticationToken: {
-					Username: "TESTEAPI.QUA",
-					Password: "TST90",
-					EnvironmentName: "QUALIVIDAPROJ"
-				},
-				Data: {
-					InterfacedoCliente: [
-						{
-							SequenciadoRegistro: 1,
-							Codigo: cliente.titular.id,
-							TipodePessoa: "F",
-							Nome: cliente.titular.nome,
-							CPFouCNPJ: cliente.titular.cpf,
-							NomeFantansia: "",
-							TipodoLocaldoIndicadordeInscricaoEstadual: "9",
-							Inscricao: "",
-							InscricaoMunicipal: "",
-							InscricaoSuframa: "",
-							OrgaoExpeditor: "",
-							DatadaExpedicao: "",
-							DatadeNascimento: dataNascimento,
-							CodigodaNacionalidade: "",
-							EstadoCivil: cliente.titular.estadoCivil.descricao,
-							Profissao: cliente.titular.profissao.descricao,
-							CodigodoGrupo: "",
-							CodigodoPais: "",
-							Cep: cliente.titular.endereco.cep,
-							Endereco: cliente.titular.endereco.logradouro,
-							NumerodoEndereco: cliente.titular.endereco.numero,
-							ComplementodoEndereco: cliente.titular.endereco.complemento,
-							Bairro: cliente.titular.endereco.bairro,
-							Uf: cliente.titular.endereco.uf,
-							Cidade: cliente.titular.endereco.cidade,
-							Email: cliente.titular.email,
-							Telefone: cliente.titular.numCelular,
-							CodigodaCidade: "3304557",
-							Ativo: "A",
-							DatadoCadastro: "",
-							DatadeAtualizacao: "",
-							DatadeInativacao: "",
-							Pais: "Brasil",
-							InterfaceContaCorrentedoCliente: [
-								{
-									SequenciadaConta: 1,
-									CodigodoCliente: cliente.titular.id,
-									CodigodaContaCorrente: "001",
-									CodigodoBanco: "341",
-									NomedoBanco: "",
-									AgenciadoBanco: "0740",
-									NomedaAgencia: "",
-									EnderecodaAgencia: "",
-									BairrodaAgencia: "",
-									CidadedaAgencia: "",
-									UFdaAgencia: "",
-									CepdaAgencia: "",
-									NumerodaContaBancaria: "62535-9",
-									TipodeConta: "",
-									Competencia: "",
-									OperacaodeIntegracao: ""
-								}
-							],
-							InterfaceEnderecodoCliente: [
-								{
-									SequenciaClienteEndereco: "1",
-									CodigoEnderecoAlternativo: "A01",
-									DescricaoEnderecoAlternativo: "Endereço de cobrança",
-									NomeCliente: cliente.titular.nome,
-									EnderecoAlernativo: "",
-									Numero: "",
-									Complemento: "",
-									Bairro: "",
-									Cidade: "",
-									UF: "RJ",
-									CEP: "",
-									Telefone: "",
-									CNPJ: "26782341859",
-									InscricaoEstadual: "",
-									CodigoRegiao: "",
-									Email: "",
-									InscricaoMunicipal: "",
-									InscricaoSUFRAMA: "",
-									CodigoCidadeIBGE: "",
-									CodigoPaisIBGE: "",
-									TipoLocalIndicadorInscricaoEstadual: "1",
-									OperacaodeIntegracao: ""
-								}
-							],
-							InterfaceContabildoCliente: [
-								{
-									CodigoCliente: cliente.titular.id,
-									CodigoEmpresa: "01",
-									CodigoFilial: "",
-									CodigoMoeda: "",
-									NumeroContaContabil: "",
-									NumeroContaContabilAntecipacao: "",
-									OperacaodeIntegracao: "",
-									InterfaceGrupoRecebimentodoCliente: [
-										{
-											CodigoCliente: cliente.titular.id,
-											CodigoEmpresa: "01",
-											CodigoFilial: "",
-											CodigoMoeda: "",
-											CodigoGrupoRecebimento: "310053",
-											CodigoImpostoIRRF: "",
-											CodigoImpostoINSS: "",
-											CodigoImpostoISS: "",
-											CodigoImpostoPIS: "",
-											CodigoImpostoCOFINS: "",
-											CodigoImpostoContribuicaoSocial: "",
-											IndicadorGrupoPrincipal: "",
-											IdentificadorTipoServico: "",
-											CodigoAtividadeEconomica: ""
-										}
-									]
-								}
-							]
-						}
-					]
-				}
-			}).then(resp => {
-				res.json({ "resposta Servidor MXM": resp.data.Messages[0], "Dados Enviados": JSON.parse(resp.config.data), "Processo :": resp.data.Data });
-			})
+			// await axios.post('https://prjqualivida.mxmwebmanager.com.br/api/InterfacedoCliente/Gravar', {
+			// 	AutheticationToken: {
+			// 		Username: "TESTEAPI.QUA",
+			// 		Password: "TST90",
+			// 		EnvironmentName: "QUALIVIDAPROJ"
+			// 	},
+			// 	Data: {
+			// 		InterfacedoCliente: [
+			// 			{
+			// 				SequenciadoRegistro: 1,
+			// 				Codigo: cliente.titular.id,
+			// 				TipodePessoa: "F",
+			// 				Nome: cliente.titular.nome,
+			// 				CPFouCNPJ: cliente.titular.cpf,
+			// 				NomeFantansia: "",
+			// 				TipodoLocaldoIndicadordeInscricaoEstadual: "9",
+			// 				Inscricao: "",
+			// 				InscricaoMunicipal: "",
+			// 				InscricaoSuframa: "",
+			// 				OrgaoExpeditor: "",
+			// 				DatadaExpedicao: "",
+			// 				DatadeNascimento: dataNascimento,
+			// 				CodigodaNacionalidade: "",
+			// 				EstadoCivil: cliente.titular.estadoCivil.descricao,
+			// 				Profissao: cliente.titular.profissao.descricao,
+			// 				CodigodoGrupo: "",
+			// 				CodigodoPais: "",
+			// 				Cep: cliente.titular.endereco.cep,
+			// 				Endereco: cliente.titular.endereco.logradouro,
+			// 				NumerodoEndereco: cliente.titular.endereco.numero,
+			// 				ComplementodoEndereco: cliente.titular.endereco.complemento,
+			// 				Bairro: cliente.titular.endereco.bairro,
+			// 				Uf: cliente.titular.endereco.uf,
+			// 				Cidade: cliente.titular.endereco.cidade,
+			// 				Email: cliente.titular.email,
+			// 				Telefone: cliente.titular.numCelular,
+			// 				CodigodaCidade: "3304557",
+			// 				Ativo: "A",
+			// 				DatadoCadastro: "",
+			// 				DatadeAtualizacao: "",
+			// 				DatadeInativacao: "",
+			// 				Pais: "Brasil",
+			// 				InterfaceContaCorrentedoCliente: [
+			// 					{
+			// 						SequenciadaConta: 1,
+			// 						CodigodoCliente: cliente.titular.id,
+			// 						CodigodaContaCorrente: "001",
+			// 						CodigodoBanco: "341",
+			// 						NomedoBanco: "",
+			// 						AgenciadoBanco: "0740",
+			// 						NomedaAgencia: "",
+			// 						EnderecodaAgencia: "",
+			// 						BairrodaAgencia: "",
+			// 						CidadedaAgencia: "",
+			// 						UFdaAgencia: "",
+			// 						CepdaAgencia: "",
+			// 						NumerodaContaBancaria: "62535-9",
+			// 						TipodeConta: "",
+			// 						Competencia: "",
+			// 						OperacaodeIntegracao: ""
+			// 					}
+			// 				],
+			// 				InterfaceEnderecodoCliente: [
+			// 					{
+			// 						SequenciaClienteEndereco: "1",
+			// 						CodigoEnderecoAlternativo: "A01",
+			// 						DescricaoEnderecoAlternativo: "Endereço de cobrança",
+			// 						NomeCliente: cliente.titular.nome,
+			// 						EnderecoAlernativo: "",
+			// 						Numero: "",
+			// 						Complemento: "",
+			// 						Bairro: "",
+			// 						Cidade: "",
+			// 						UF: "RJ",
+			// 						CEP: "",
+			// 						Telefone: "",
+			// 						CNPJ: "26782341859",
+			// 						InscricaoEstadual: "",
+			// 						CodigoRegiao: "",
+			// 						Email: "",
+			// 						InscricaoMunicipal: "",
+			// 						InscricaoSUFRAMA: "",
+			// 						CodigoCidadeIBGE: "",
+			// 						CodigoPaisIBGE: "",
+			// 						TipoLocalIndicadorInscricaoEstadual: "1",
+			// 						OperacaodeIntegracao: ""
+			// 					}
+			// 				],
+			// 				InterfaceContabildoCliente: [
+			// 					{
+			// 						CodigoCliente: cliente.titular.id,
+			// 						CodigoEmpresa: "01",
+			// 						CodigoFilial: "",
+			// 						CodigoMoeda: "",
+			// 						NumeroContaContabil: "",
+			// 						NumeroContaContabilAntecipacao: "",
+			// 						OperacaodeIntegracao: "",
+			// 						InterfaceGrupoRecebimentodoCliente: [
+			// 							{
+			// 								CodigoCliente: cliente.titular.id,
+			// 								CodigoEmpresa: "01",
+			// 								CodigoFilial: "",
+			// 								CodigoMoeda: "",
+			// 								CodigoGrupoRecebimento: "310053",
+			// 								CodigoImpostoIRRF: "",
+			// 								CodigoImpostoINSS: "",
+			// 								CodigoImpostoISS: "",
+			// 								CodigoImpostoPIS: "",
+			// 								CodigoImpostoCOFINS: "",
+			// 								CodigoImpostoContribuicaoSocial: "",
+			// 								IndicadorGrupoPrincipal: "",
+			// 								IdentificadorTipoServico: "",
+			// 								CodigoAtividadeEconomica: ""
+			// 							}
+			// 						]
+			// 					}
+			// 				]
+			// 			}
+			// 		]
+			// 	}
+			// }).then(resp => {
+			// 	res.json({ "resposta Servidor MXM": resp.data.Messages[0], "Dados Enviados": JSON.parse(resp.config.data), "Processo :": resp.data.Data });
+			// })
 		}
 	});
 };
@@ -239,7 +240,7 @@ function retornaCampo(status, CampoSelecionado) {
 		case 5:
 			let entidade
 			if (CampoSelecionado)
-				entidade = { "entidade.nome": CampoSelecionado }
+				entidade = { "entidade.sigla": CampoSelecionado }
 
 			else
 				entidade = {}
@@ -260,6 +261,13 @@ var getFindContrato = function (req, res, next) {
 	let dataNascimento = req.query.dataNascimento;
 	let nomeTitular = req.query.nomeTitular;
 	let entidade = req.query.entidade;
+	let skip = req.query.skip;
+	// let dataNascimentoFormat = moment(dataNascimento).format('')
+
+	// let cliente = new ICliente() 
+	// console.log('iCliente: ', cliente )
+	// console.log('dataNascimento: ', dataNascimentoFormat)
+
 
 	if (administradora || operadora || dataNascimento || nomeTitular || entidade) {
 
@@ -269,7 +277,8 @@ var getFindContrato = function (req, res, next) {
 		let quarto = retornaCampo(4, nomeTitular)
 		let quinto = retornaCampo(5, entidade);
 
-		Contrato.find({ $and: [primeiro, segundo, terceiro, quarto, quinto] }).then(resp => {
+		Contrato.find({ $and: [primeiro, segundo, terceiro, quarto, quinto] }).skip(1).limit(1)
+		.then(resp => {
 			res.json(resp);
 		})
 	} else {
@@ -279,7 +288,7 @@ var getFindContrato = function (req, res, next) {
 			} else {
 				res.json(contratos);
 			}
-		});
+		}).skip(parseInt(skip)).limit(1);
 	}
 };
 
